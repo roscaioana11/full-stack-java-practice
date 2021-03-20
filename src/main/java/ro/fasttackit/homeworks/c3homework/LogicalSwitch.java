a2c63255c5367dfc97e8c18c1c976b5139b6405c
rosca_ioana <rosca.ioana11@gmail.com>
2021-03-20 18:58:34 +0200

    Homework 3 - Second Exercise Fixed LogicalSwitch

--- src/main/java/ro/fasttackit/homeworks/c3homework/LogicalSwitch.java
+++ src/main/java/ro/fasttackit/homeworks/c3homework/LogicalSwitch.java
@@ -1,10 +1,29 @@
 package ro.fasttackit.homeworks.c3homework;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+import static java.util.stream.Collectors.toList;
+
 public class LogicalSwitch {
-    public static String switchPerson(Predicate<Person> predicatePerson,Function<Predicate<Person>, String> functionPredicatePerson){
-        return functionPredicatePerson.apply(predicatePerson);
+    private Map<Predicate<Person>, Function<Predicate<Person>, String>> switchMap;
+
+    public LogicalSwitch() {
+        this.switchMap = new HashMap<>();
+    }
+
+    public void put(Predicate<Person> predicate, Function<Predicate<Person>, String> function){
+        switchMap.put(predicate, function);
+    }
+
+    public List<String> process(Person person){
+        return switchMap.keySet().stream()
+                .filter(predicate -> predicate.test(person))
+                .map(predicate -> switchMap.get(predicate).apply(predicate))
+                .collect(toList());
+        //return switchMap.get(predicate).apply(predicate);
     }
 }
--- src/main/java/ro/fasttackit/homeworks/c3homework/Test.java
+++ src/main/java/ro/fasttackit/homeworks/c3homework/Test.java
@@ -4,31 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * LogicalSwitch simuleaza un switch/case in sensul ca acesta verifica daca o anumita conditie este indeplinita pentru a executa o functie
+ * LogicalSwitch simuleaza un switch/case
+ * acesta primeste un parametru persoana si aplica tot felul de teste pe aceasta persoana, returnand numa rezultatul testelor care au avut succes
  */
 public class Test {
     public static void main(String[] args) {
-        Person person = new Person("Ioana", 25, "Cluj-Napoca");
         List<Person> people = new ArrayList<>();
         people.add(new Person("Ion", 35, "Arad"));
-        people.add(new Person("Vlad", 16, "Otopeni"));
+        people.add(new Person("Vlad", 31, "Otopeni"));
+        people.add(new Person("Ioana", 25, "Cluj-Napoca"));
 
-        String message = LogicalSwitch.switchPerson(predicate -> !people.contains(predicate), personPredicate -> {
-            if(personPredicate.test(person)){
-                people.add(person);
-                return "Added Person";
-            }
-            return "Person already exists";
-        });
-        System.out.println(message);
-        //test if exists
-        message = LogicalSwitch.switchPerson(predicate -> !people.contains(predicate), personPredicate -> {
-            if(personPredicate.test(person)){
-                people.add(person);
-                return "Added Person";
-            }
-            return "Person already exists";
-        });
-        System.out.println(message);
+        LogicalSwitch logicalSwitch = new LogicalSwitch();
+        logicalSwitch.put(person -> person.getName().length() >= 4,
+                predicate -> "Lungimea numelui este mai mare ca si 3 caractere");
+        logicalSwitch.put(person -> person.getAge() < 30,
+                predicate -> "Varsta persoanei este mai mica de 30");
+
+        List<String> messages = logicalSwitch.process(people.get(0));
+        messages.stream()
+                .forEach(message -> System.out.println(message));
+
+        messages = logicalSwitch.process(people.get(1));
+        messages.stream()
+                .forEach(message -> System.out.println(message));
+
+        System.out.println("");
+
+        messages = logicalSwitch.process(people.get(2));
+        messages.stream()
+                .forEach(message -> System.out.println(message));
     }
 }
